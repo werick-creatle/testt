bc9a42f1041bc4c0fd33755de20c17507c4e0f29
werick-creatle <werick.luiz16@gmail.com>
2024-10-23 19:37:00 -0300

    Add files via upload

---
+++ exercicio 6.java
@@ -0,0 +1,36 @@
+// exercicio 6:
+public class Main {
+    public static void main(String[] args) {
+        maiorValor();
+        menorNumero();
+        somaDivisao();
+    }
+
+    public static void maiorValor() {
+        int[] vetor = { 1, 2, 3, 4 };
+        int maior = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            if (maior < vetor[i]) {
+                maior = vetor[i];
+            }
+        }
+        System.out.println("O maior número é: " + maior);
+    }
+
+    public static void menorNumero() {
+        int[] vetor = { 2, 4, 6, 8 };
+        int menor = 0;
+        for (int i = 0; i > vetor.length; i++) {
+            if (menor > vetor[i]) {
+                menor = vetor[i];
+            }
+        }
+        System.out.println("O menor número é: " + menor);
+    }
+
+    public static void somaDivisao() {
+        int[] vetor = { 2, 4, 6, 8 };
+        int res = (vetor[0] + vetor[3]) / 2;
+        System.out.println("resultado da soma e depois divisao é: " + res);
+    }
+}
