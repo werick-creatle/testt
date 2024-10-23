26542eb75219f717b0471f30b97bb0fd3b2253df
werick-creatle <werick.luiz16@gmail.com>
2024-10-23 20:35:53 -0300

    Add files via upload

---
+++ exercicio 7.java
@@ -0,0 +1,20 @@
+
+// esercicio 7:
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        calcularFatorial();
+    }
+
+    public static void calcularFatorial() {
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Digite um número para saber o fatorial: ");
+        int num = ler.nextInt();
+        int fat = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fat = fat * i;
+        }
+        System.out.println("O resultado do fatoria de " + num + " é: " + fat);
+    }
+}
