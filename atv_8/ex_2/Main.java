27d2a00b99126d54e2b1d63df014443c76302c52
werick-criatle <werick.luiz16@gmail.com>
2024-10-24 11:26:21 -0300

    atividade 8 ex_2

---
+++ atv_8/ex_2/Main.java
@@ -0,0 +1,22 @@
+package atv_8.ex_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Ponto p1 = new Ponto();
+        Ponto p2 = new Ponto();
+        Ponto p3 = new Ponto();
+        Ponto p4 = new Ponto();
+
+        p1.coordenadaX = 0;
+        p1.coordenadaY = 0;
+
+        p2.coordenadaX = 2;
+        p2.coordenadaY = 2;
+
+        p3.coordenadaX = 0;
+        p3.coordenadaY = 2;
+
+        p4.coordenadaX = 2;
+        p4.coordenadaY = 2;
+    }
+}
---
+++ atv_8/ex_2/Ponto.java
@@ -0,0 +1,9 @@
+package atv_8.ex_2;
+
+public class Ponto {
+
+    double coordenadaX;
+    double coordenadaY;
+
+    
+}
